1cb8eea2c94347b70dc678314863fbcc36d46143
The world of code <121226086+SCCXLyyds@users.noreply.github.com>
2023-01-27 10:09:01 +0800

    Update 一维数组前缀和（prefix and）.cpp

--- 一维数组前缀和（prefix and）.cpp
+++ 一维数组前缀和（prefix and）.cpp
@@ -36,30 +36,5 @@ int main()
     }
     return 0;
 }
-//二维数组前缀和：
-#include <iostream>
-
-using namespace std;
-const int N=1001;
-int sum[N][N],a[N][N],x,y,m,x1,x2,y1,y2;
-int main()
-{
-	scanf("%d%d%d",&x,&y,&m);  //x<=l<=r<=L<=R<=y
-	for(int i=1;i<=x;i++){
-		for(int j=1;j<=y;j++){
-			scanf("%d",&a[i][j]);
-		}
-	}
-	for(int i=1;i<=x;i++){
-			for(int j=1;j<=y;j++){
-				sum[i][j]=sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+a[i][j];
-			}
-		}
-	while(m--){
-	scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
-		printf("%d\n",sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]);
-	}
-	return 0;
-}
 
  
